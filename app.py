ba36e00b443bd98373c66b75af96869e75701911
DMVance <csljrrt@gmail.com>
2021-03-28 19:11:12 -0500

    edit import russ_map from uss_russell.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import plotly.graph_objects as go
 import os
 from datetime import datetime
 # from config import api_key, mapbox_token
-from uss_russell.py import russ_map
+from uss_russell import russ_map
 
 app = Flask(__name__)
 
