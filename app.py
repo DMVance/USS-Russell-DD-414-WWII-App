9e426048b31c4ac17840807ccd50ffb6b4d8e9c5
DMVance <csljrrt@gmail.com>
2021-03-29 22:00:34 -0500

    switch from russ.html to index.html, which extends parent.html

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     fig = russ_map()
-    return render_template("russ.html", fig=fig)
+    return render_template("index.html", fig=fig)
 
 # @app.route("/maps")
 # def map():
