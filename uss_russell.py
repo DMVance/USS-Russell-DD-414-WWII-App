b5558b0a3640001c4fd960730c219d5ce6cf2804
DMVance <csljrrt@gmail.com>
2021-03-30 20:20:09 -0500

    resize plot

--- uss_russell.py
+++ uss_russell.py
@@ -159,7 +159,7 @@ def russ_map():               # Have to use JS format to render on webpage? This
         hover_data=["date", "location", "comment"],
         title="Events and Places of the USS Russell DD 414 - WWII",
         mapbox_style="basic",
-        width=1100,
+        width=1250,
         height=900,
         animation_frame="event_date",  # The single line that brings animation to the map based on the parameter indicated
     )
